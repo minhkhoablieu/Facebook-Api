831f9a06048cb38223794cacf81a33a41e47de35
Khoa Phung <chanzi909@gmail.com>
2017-12-07 22:17:18 +0700

    Update botv2.py
    Update LOGIC =)) and add some BUG

--- botv2.py
+++ botv2.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python
 import requests, json, time, random, operator, heapq
-count_react = {'like': 0,'love': 0,'haha': 0,'wow': 0,'sad': 0,'angry': 0,}
-
+count_react = {'LIKE': 0,'LOVE': 0,'HAHA': 0,'WOW': 0,'SAD': 0,'ANGRY': 0,}
+react = ''
 def like(idlike, token):
- 
+    time.sleep(2)
     payload = {'method': 'GET', 'access_token':token}
     get_react = requests.get('https://graph.facebook.com/v2.10/'+idlike+'?fields=reactions.type(LIKE).limit(0).summary(1).as(like),reactions.type(LOVE).limit(0).summary(1).as(love),reactions.type(HAHA).limit(0).summary(1).as(haha),reactions.type(WOW).limit(0).summary(1).as(wow),reactions.type(SAD).limit(0).summary(1).as(sad),reactions.type(ANGRY).limit(0).summary(1).as(angry)&limit=10', payload).json()
-    count_react['like']=get_react['like']['summary']['total_count']
-    count_react['love']=get_react['love']['summary']['total_count']
-    count_react['haha']=get_react['haha']['summary']['total_count']
-    count_react['wow']=get_react['wow']['summary']['total_count']
-    count_react['sad']=get_react['sad']['summary']['total_count']
-    count_react['angry']=get_react['angry']['summary']['total_count']
+    count_react['LIKE']=get_react['like']['summary']['total_count']
+    count_react['LOVE']=get_react['love']['summary']['total_count']
+    count_react['HAHA']=get_react['haha']['summary']['total_count']
+    count_react['WOW']=get_react['wow']['summary']['total_count']
+    count_react['SAD']=get_react['sad']['summary']['total_count']
+    count_react['ANGRY']=get_react['angry']['summary']['total_count']
     reaction = sorted(count_react, key=count_react.__getitem__)[4]
-    if count_react[reaction] == 0:
-        reaction = 'LIKE'
-    payload = {'type':reaction.upper(), 'method': 'POST' ,'access_token':token}
+    reaction2 = sorted(count_react, key=count_react.__getitem__)[5]
+    if count_react[reaction] != 0 and count_react[reaction2] != 0:
+        if reaction == 'LIKE':
+            react = reaction2
+        else:
+            react = reaction
+    elif count_react[reaction] == 0 and count_react[reaction2] != 0:
+        react = reaction2
+    else:
+        react = 'LOVE'
+    payload = {'type':react.upper(), 'method': 'POST' ,'access_token':token}
     a = requests.post('https://graph.facebook.com/v2.8/'+idlike+'/reactions', params=payload)
-    print idlike ,reaction.upper() , count_react
-
+    print '%-35s%-6s%-6s%-5i%-6s%-5s' % (idlike,react,reaction,count_react[reaction],reaction2,count_react[reaction2])
 token = ''
 next_id = ['364997627165697', '100003880469096', '100010362521296','100005179329159','100009618621937']
 id = 0
@@ -28,7 +35,14 @@ while True:
     for i in t['data']:
         if i['id'] == id:   break
         if i['id'][:15] not in next_id:
-            like(i['id'], token)
-    print 'chua co stt moi'
+            payload = {'fields':'gender','method': 'POST' ,'access_token':token}
+            a = requests.post('https://graph.facebook.com/v2.8/'+i['id'][:15], params=payload).json()
+            try:
+                if a['gender'] == 'famele':
+                    next
+                else:
+                    like(i['id'], token)
+            except KeyError:            
+                like(i['id'], token)
     time.sleep(10)
     id = t['data'][0]['id']
